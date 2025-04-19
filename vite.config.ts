91f265d7eeef94f2bd09d3582084b0f2554f7a79
Michaël De Boey <info@michaeldeboey.be>
2025-04-20 00:00:00 +0200

    fix: workaround for Prisma Client issue in browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
+import { createRequire } from "node:module";
+import path from "node:path";
+
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const require = createRequire(import.meta.url);
+const prismaClientPath = require
+  .resolve("@prisma/client")
+  .replace(/@prisma(\/|\\)client(\/|\\).*/, ".prisma/client");
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // This is a workaround for the issue with Prisma Client and Vite
+      // where the client is not being properly resolved in the browser
+      // See: https://github.com/prisma/prisma/issues/12504
+      ".prisma/client/index-browser": path.join(
+        prismaClientPath,
+        "index-browser.js",
+      ),
+    },
+  },
 });
